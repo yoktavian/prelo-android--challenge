0348bff1ddf9c3586d234d94d436d2d60941f78c
yoktavian <yoktavian17@gmail.com>
2017-12-11 15:59:42 +0700

    push

---
+++ app/src/main/java/yoktavian/dev/prelo/ActvitiyLogin.java
@@ -0,0 +1,13 @@
+package yoktavian.dev.prelo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActvitiyLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actvitiy_login);
+    }
+}
